28652ae8ce6790fc415618a0326f023bde3e7efe
ericjohnson97 <eric.emj.johnson@gmail.com>
2017-06-05 20:50:41 -0500

    cleaned multitracking code a little. need to figure out how to compile with the headers

---
+++ multi/CMakeFiles/multiTarget.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multiTarget.dir/multiTracking.cpp.o"
+  "multiTarget.pdb"
+  "multiTarget"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multiTarget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
