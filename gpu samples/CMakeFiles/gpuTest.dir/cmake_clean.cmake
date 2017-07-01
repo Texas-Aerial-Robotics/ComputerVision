d6b509bdf01595ee3a6344a19b3973d02549f200
Mark Loveland <markloveland@utexas.edu>
2017-07-01 13:20:33 -0500

    GPU

---
+++ gpu samples/CMakeFiles/gpuTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gpuTest.dir/gpuTest.cpp.o"
+  "gpuTest.pdb"
+  "gpuTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gpuTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- gpu samples/gpuTest.cpp
+++ gpu samples/gpuTest.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include "opencv2/opencv.hpp"
-#include "opencv2/gpu/gpu.hpp"
-
+#include "opencv2/core/cuda.hpp"
+#include <vector>
+#include <opencv2/imgproc.hpp>
+using namespace cv;
+using namespace std;
+#include <iostream>
 int main (int argc, char* argv[])
 {
     try
     {
         cv::Mat src_host = cv::imread("index.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-        cv::gpu::GpuMat dst, src;
+        cv::cuda::GpuMat dst, src;
         src.upload(src_host);
 
-        cv::gpu::threshold(src, dst, 128.0, 255.0, CV_THRESH_BINARY);
+       // cv::cuda::threshold(src, dst, 128.0, 255.0, CV_THRESH_BINARY);
 
         cv::Mat result_host;
-        dst.download(result_host);
+        src.download(result_host);
 
         cv::imshow("Result", result_host);
         cv::waitKey();
